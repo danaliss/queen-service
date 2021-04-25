cadc35d3b893925f2c802d0ac973bbe3289ec01e
Dana Liss <danaliss@gmail.com>
2021-04-25 09:57:18 -0400

    adds java project scaffold

---
+++ src/main/java/com/danaliss/queenservice/QueenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.danaliss.queenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danaliss/queenservice/QueenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danaliss.queenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
